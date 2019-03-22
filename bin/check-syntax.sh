bcd4ca9eab9541eef7c3f1bbc73d25929f809717
Tim van Dijen <tvdijen@gmail.com>
2019-03-22 21:36:50 +0100

    Upgrade dependencies / PHP >= 5.6

--- bin/check-syntax.sh
+++ bin/check-syntax.sh
@@ -4,7 +4,7 @@ PHP='/usr/bin/env php'
 RETURN=0
 
 # check PHP files
-for FILE in `find config-templates lib www -name "*.php"`; do
+for FILE in `find config-templates lib tests www -name "*.php"`; do
     $PHP -l $FILE > /dev/null 2>&1
     if [ $? -ne 0 ]; then
         echo "Syntax check failed for ${FILE}"
