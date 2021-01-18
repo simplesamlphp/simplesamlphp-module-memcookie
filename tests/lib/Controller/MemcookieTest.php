ec2cf9cfaa055c8ab009af2f005f879b965a6c4a
Tim van Dijen <tvdijen@gmail.com>
2021-01-18 23:03:40 +0100

    Add unit test

--- tests/lib/Controller/MemcookieTest.php
+++ tests/lib/Controller/MemcookieTest.php
@@ -5,17 +5,12 @@ declare(strict_types=1);
 namespace SimpleSAML\Test\Module\memcookie\Controller;
 
 use PHPUnit\Framework\TestCase;
-//use SimpleSAML\Auth\Source;
-//use SimpleSAML\Auth\State;
 use SimpleSAML\Configuration;
 use SimpleSAML\Error;
 use SimpleSAML\HTTP\RunnableResponse;
-//use SimpleSAML\Module\multiauth\Auth\Source\MultiAuth;
 use SimpleSAML\Module\memcookie\Controller;
 use SimpleSAML\Session;
-//use SimpleSAML\XHTML\Template;
 use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Set of tests for the controllers in the "memcookie" module.
@@ -25,7 +20,13 @@ use Symfony\Component\HttpFoundation\Request;
 class MemcookieTest extends TestCase
 {
     /** @var \SimpleSAML\Configuration */
+    protected $authsources;
+
+    /** @var \SimpleSAML\Configuration */
     protected $config;
+
+    /** @var \SimpleSAML\Configuration */
+    protected $module_config;
 
     /** @var \SimpleSAML\Session */
     protected $session;
@@ -47,6 +48,30 @@ class MemcookieTest extends TestCase
             'simplesaml'
         );
 
+
+        $this->authsources = Configuration::loadFromArray(
+            [
+                'default-sp' => ['saml:SP'],
+            ],
+            '[ARRAY]',
+            'authsources.php'
+        );
+        Configuration::setPreLoadedConfig($this->authsources, 'authsources.php');
+
+        $this->module_config = Configuration::loadFromArray(
+            [
+                'authsource' => 'default-sp',
+                'cookiename' => 'AuthMemCookie',
+                'username' => null,
+                'groups' => null,
+                'memcache.host' => '127.0.0.1',
+                'memcache.port' => 11211,
+            ],
+            '[ARRAY]',
+            'module_authmemcookie.php'
+        );
+        Configuration::setPreLoadedConfig($this->module_config, 'module_authmemcookie.php');
+
         $this->session = Session::getSessionFromRequest();
     }
 
@@ -57,13 +82,18 @@ class MemcookieTest extends TestCase
      */
     public function testMemcookie(): void
     {
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['REQUEST_URI'] = '/module.php/memcookie/';
+        $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+
         $request = Request::create(
-            '/',
+            '/module.php/memcookie/',
             'GET',
             []
         );
 
         $c = new Controller\Memcookie($this->config, $this->session);
+        /** @var \SimpleSAML\HTTP\RunnableResponse $response */
         $response = $c->main($request);
 
         $this->assertInstanceOf(RunnableResponse::class, $response);
